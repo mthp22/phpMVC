9e94a261c8c7a7413dc7eb1f12fc3116b0a8b9a4
mthp22 <221026639@students.uj.ac.za>
2026-04-07 15:38:28 +0200

    config file template

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'dev-mariadb-1');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'shareposts');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost:/shareposts/');
+  // Site Name
+  define('SITENAME', 'SPs');
+
+  define('APPVERSION','1.0.0');
